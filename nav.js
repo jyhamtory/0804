406d926b0d77d336136e661edd378f1c3cfdd19d
jyhamtory <140800004+jyhamtory@users.noreply.github.com>
2023-09-13 17:47:31 +0900

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,40 @@
+$(function () {
+  $(".sub").css("display", "none");
+  
+  // 네비게이션 All menu
+  $(".all_menu").css("display", "none");
+  $("#all_menu").mouseover(function () {
+    $(this).find(".sub").stop().show();
+    $(".sub li").mouseover(function () {
+      $(".sub li").css("background-color", "#fff");
+      $(this).css("background-color", "#dfdfdf");
+    });
+  }).mouseout(function () {
+    $(this).find(".sub").stop().hide();
+    $(".sub li").css("background-color", "#fff");
+  })
+
+  // 네비게이션 브랜드 
+  $(".menu_icon").mouseover(function () {
+    $(this).find(".sub_large").stop().show();
+    $(".sub_large li").mouseover(function () {
+      $(".sub_large li").css("background-color", "#fff");
+      $(this).css("background-color", "#dfdfdf");
+    });
+  }).mouseout(function () {
+    $(this).find(".sub_large").stop().hide();
+    $(".sub_large li").css("background-color", "#fff");
+  })
+
+  // 네비게이션 브랜드 - 아울렛
+  $(".menu_icon").mouseover(function () {
+    $(this).find(".sub").stop().show();
+    $(".sub li").mouseover(function () {
+      $(".sub li").css("background-color", "#fff");
+      $(this).css("background-color", "#dfdfdf");
+    });
+  }).mouseout(function () {
+    $(this).find(".sub").stop().hide();
+    $(".sub li").css("background-color", "#fff");
+  })
+});
